d5ea0266614bf66feed2c3c2912a0ef81a2d87c4
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-20 12:24:02 +0100

    salvo l'esercizio dell'ordine dei caratteri inverso

---
+++ carattereinordineinverso.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string carattere;
+    cout << "inserisci una stringa di caratteri" << endl;
+    cin >> carattere;
+    for (int i = carattere.length(); i <= carattere.length(); i--)
+        cout << carattere[i - 1] << endl;
+}
